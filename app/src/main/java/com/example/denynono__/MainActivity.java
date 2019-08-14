445f0be73bfd2509dd329b0741a7627f52a20d22
김종완 <kimdeiz@naver.com>
2019-08-14 14:39:48 +0900

    번호판 촬영 opencv 연동 카메라 키기 성공

--- app/src/main/java/com/example/denynono__/MainActivity.java
+++ app/src/main/java/com/example/denynono__/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.denynono__;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,14 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
 //        // Example of a call to a native method
 //        TextView tv = findViewById(R.id.sample_text);
 //        tv.setText(stringFromJNI());
+    }
+    public void onClick_carNumber(View view)
+    {
+        Intent intent = new Intent(this, Camera.class);
+        startActivity(intent);
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
 }
